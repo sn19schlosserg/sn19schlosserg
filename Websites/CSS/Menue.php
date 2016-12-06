264d5e483d1f652a05cc4f6494f92d8c0c676730
sn19schlosserg <sn19.schlosserg@spartandocs.org>
2016-12-05 19:39:27 -0500

    Add files via upload

---
+++ Websites/CSS/Menue.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet"type="text/css"href="Menu.css"/>
+<link rel="stylesheet"type="text/css"href="MainBody.css"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Menu</title>
+</head>
+<body>
+<div id="MenuHolder">
+<nav>
+<ul>
+<li><a href="#">Clean-Out</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Browse</a></li>
+<li><a href="#">Donate</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</nav>
+</div>
+</body>
+</html>
